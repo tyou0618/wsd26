7bd60c719648e1fcd9f1aa97d3e6a1281749086b
tyou0618 <e1923044.oit.ac.jp>
2026-04-22 13:51:33 +0900

    add

--- myfirstapp/lib/main.dart
+++ myfirstapp/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(const MyApp());
